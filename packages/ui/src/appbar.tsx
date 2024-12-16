58c6761147cea0f20fa64bfa5c68928dc77952a2
AaraV <aaravrawat00000@gmail.com>
2024-12-16 12:00:00 +0530

    name change

--- packages/ui/src/appbar.tsx
+++ packages/ui/src/appbar.tsx
@@ -18,7 +18,7 @@ export const Appbar = (
   
     return <div className="flex justify-between border-b px-4">
     <div className="text-2xl flex flex-col justify-center text-[#6a51a6] font-bold">
-        AssPay
+        Wallet
     </div>
     <div className="flex flex-col justify-center pt-2">
         <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={user ? onSignOut : onSignIn}> {user ? "Logout" : "Login"} </button>
