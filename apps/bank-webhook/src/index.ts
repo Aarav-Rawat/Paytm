44982fb4ec38bd7bc47113ba0b42821debb2f554
AaraV <aaravrawat00000@gmail.com>
2024-12-31 20:51:40 +0530

    dummy data

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,6 +1,36 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client"
+const db = new PrismaClient();
 const app = express();
 
-app.post("/hdfcWebhook",(req,res)=>{
-    
+app.post("/hdfcWebhook",async (req,res)=>{
+    const paymentInfo = {
+        userId: req.body.id,
+        amount: req.body.amount,
+        token: req.body.token
+    }
+
+   await db.balance.update({ 
+        where:{
+            userId: paymentInfo.userId
+        },
+        data:{
+            amount:{
+                increment: paymentInfo.amount
+            }
+        }
+    })
+
+     await db.onRampTransaction.update({
+        where:{
+            token: paymentInfo.token
+        },
+        data:{
+            status: "Success"
+        }
+     })
+
+     res.status(200).json({
+        message: "captured"
+     })
 })
