730266bf2c34192a6b91a202789a773baf859430
AaraV <aaravrawat00000@gmail.com>
2025-01-01 18:55:57 +0530

    addMoneycard ui

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -1,13 +1,34 @@
 "use client"
+import { Card } from "@repo/ui/card";
+import { Select } from "@repo/ui/select";
+import { TextInput } from "@repo/ui/textinput"
 import { useState } from "react"
 
-export function AddMoney(){
+const SUPPORTED_BANKS = [{
+    name: "HDFC Bank",
+    redirectUrl: "https://netbanking.hdfcbank.com"
+}, {
+    name: "Axis Bank",
+    redirectUrl: "https://www.axisbank.com/"
+}];
+
+export function AddMoney() {
     const [amount, setAmount] = useState(0);
 
-    return
-    <div>
-        <div>
-                
-        </div>
-    </div>
+    return (
+        <Card title="Add Money">
+            <div>
+                <TextInput label={"Amount"} placeholder={"Amount"} onChange={(val) => setAmount(Number(val))} />
+
+                <div className="py-4 text-left">
+                    Bank
+                </div>
+
+           <Select options={SUPPORTED_BANKS.map(x => ({
+            key: x.name,
+            value: x.name
+        }))} />
+            </div>
+        </Card>
+    )
 }
