09eb3c1262b2c84ea94e15b2d2ee5f1d3063ea27
AaraV <aaravrawat00000@gmail.com>
2025-01-02 21:56:02 +0530

    fixing webhook bug

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -3,44 +3,53 @@ import { PrismaClient } from "@prisma/client"
 const db = new PrismaClient();
 const app = express();
 
-app.post("/hdfcWebhook",async (req,res)=>{
-    const paymentInfo = {
-        userId: req.body.id,
-        amount: req.body.amount,
-        token: req.body.token
+app.use(express.json())
+
+app.post("/hdfcWebhook", async (req, res) => {
+   
+    const paymentInformation: {
+        token: string;
+        userId: string;
+        amount: string
+    } = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                       
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.updateMany({
+                where: {
+                    token: paymentInformation.token
+                }, 
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    } catch(e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
     }
 
-try{
-    await db.balance.update({ 
-    
-        where:{
-            userId: paymentInfo.userId
-        },
-        data:{
-            amount:{
-                increment: paymentInfo.amount
-            }
-        }
-    })
-
-     await db.onRampTransaction.update({
-        where:{
-            token: paymentInfo.token
-        },
-        data:{
-            status: "Success"
-        }
-     })
-
-     res.status(200).json({
-        message: "captured"
-     })
-}
-catch(err){
-    console.log(err);
-    res.status(411).json({
-        message: "error while payment processing webhook"
-    })
-}
-  
 })
+
+app.listen(3002);
