d9df697eb97dfaba42c14d35b38fdd3c2ab7f508
AaraV <aaravrawat00000@gmail.com>
2025-01-04 13:21:29 +0530

    changed name

--- packages/ui/src/appbar.tsx
+++ packages/ui/src/appbar.tsx
@@ -18,8 +18,8 @@ export const Appbar = (
 ) => {
   
     return <div className="flex justify-between border-b px-4">
-    <div className="text-lg flex flex-col justify-center">
-        Paytm
+    <div className="text-2xl flex flex-col justify-center text-[#6a51a6] font-bold">
+        AssPay
     </div>
     <div className="flex flex-col justify-center pt-2">
         <Button onClick={user ? onSignOut : onSignIn}>{user ? "Logout" : "Login"}</Button>
