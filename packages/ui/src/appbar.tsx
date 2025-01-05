84fef34ed05d09f21b4cc0d462022fd3a43ded42
AaraV <aaravrawat00000@gmail.com>
2025-01-05 21:23:25 +0530

    created dockerfiler and checked it

--- packages/ui/src/appbar.tsx
+++ packages/ui/src/appbar.tsx
@@ -1,5 +1,4 @@
 
-import { Button } from './button.js'
 
 export const Appbar = (
     {
@@ -22,7 +21,8 @@ export const Appbar = (
         AssPay
     </div>
     <div className="flex flex-col justify-center pt-2">
-        <Button onClick={user ? onSignOut : onSignIn}>{user ? "Logout" : "Login"}</Button>
+        <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={user ? onSignOut : onSignIn}> {user ? "Logout" : "Login"} </button>
+      
     </div>
 </div>
   
